f7f84016d6be67c88075fbd5e3deab759d1c3ddb
Chris Collins <ccollins36@icloud.com>
2020-08-25 08:30:52 -0400

    finshed readme section

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -3,6 +3,20 @@ function generateMarkdown(data) {
   return `# ${data.title}
 ## Description
  ${data.description}
+ ## Table of Contents
+ ${data.tableofcontents}
+ ## Installation
+ ${data.installation}
+ ## Usage
+ ${data.usage}
+ ## License
+ ${data.license}
+ ## Contributing
+ ${data.contributing}
+ ## Tests
+ ${data.tests}
+ ## Questions
+ ${data.questions}
 `;
 }
 
